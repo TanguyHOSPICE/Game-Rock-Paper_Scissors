99ff94c232a9db0bc4f896495ed688f749735ec7
tanguy hospice <hospicetanguy@gmail.com>
2021-05-17 09:26:43 -0400

    Add playMatch function

--- app.js
+++ app.js
@@ -4,18 +4,37 @@ const game = () => {
 
 	//Start the Game
 	const startGame = () => {
+		const playBtn = document.querySelector(".intro button");
 		const introScreen = document.querySelector(".intro");
 		const match = document.querySelector(".match");
-		const playBtn = document.querySelector(".intro button");
 
 		playBtn.addEventListener("click", () => {
 			introScreen.classList.add('fadeOut');
 			match.classList.add('fadeIn');
 		});
 	};
+	//Play Match
+	const playMatch = () => {
+		const options = document.querySelectorAll(".options button");
+		const playerHand = document.querySelector(".player-hand");
+		const computerHand = document.querySelector(".computer-hand");
+		//Computer Options
+		const computerOptions = ['rock','paper','scissors'];
+
+		//Select Options
+		options.forEach(option => {
+			option.addEventListener("click", function () {
+				const computerNumber = Math.floor(Math.random() * 3);
+				const computerChoice = computerOptions[computerNumber];
+				//Here is where we call compare hands
+			});
+		});
+	};
+
 	//Is call all the inner function
 	startGame();
-	updateScore();
+	playMatch();
+	// updateScore();
 };
 //start the game function
 game();
